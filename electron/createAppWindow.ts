9dc9777a592cf967f6e41f14d98337d3130abf62
shanejonas <jonas.shane@gmail.com>
2019-11-01 08:29:07 -0700

    fix: remove menubar and just use electron

---
+++ electron/createAppWindow.ts
@@ -0,0 +1,24 @@
+import { BrowserWindow } from "electron";
+import * as path from "path";
+
+export function createAppWindow() {
+  let win = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  if (process.env.NODE_ENV === "development") {
+    win.loadURL("http://localhost:3000/index.html");
+    // Open the DevTools.
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(`file://${path.join(__dirname, "../build/index.html")}`);
+  }
+
+  win.on("closed", () => {
+    win = null;
+  });
+}
--- electron/electron.ts
+++ electron/electron.ts
@@ -1,12 +1,20 @@
-import { menubar } from "menubar";
-import * as path from "path";
+import { app } from "electron";
+import { createAppWindow } from "./createAppWindow";
 
-const mb = menubar({
-  index: process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/index.html"
-    : `file://${path.join(__dirname, "../build/index.html")}`,
-});
+async function showWindow() {
+  try {
+    return createAppWindow();
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-mb.on("ready", () => {
-  console.log("app is ready") //tslint:disable-line
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", showWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  app.quit();
 });
